8297448574b2c4391b81b9e44ee413e91e3eb62d
Javier Javier Daza Olivella <jdaza@internal-admins-internal-traffic-l-6ad629-1124484131.us-east-1.elb.amazonaws.com>
2026-03-24 12:35:25 -0500

    add

--- javiface/__init__.py
+++ javiface/__init__.py
@@ -1,5 +1,7 @@
 import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
-from javiface.JaviFace import JaviFace as JaviFace
+from javiface.JaviFace import FaceVerifier as JaviFace
+
+__all__ = ["JaviFace"]
 
 
 """
